0c8f58e594e19ee8271b4621130cc2abc680c0a9
Vishnu u <programmer.uv@gmail.com>
2019-03-11 09:58:08 +0530

    initial push

--- jsonParserExample/ViewController.swift
+++ jsonParserExample/ViewController.swift
@@ -12,7 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "jsonparse", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            guard let basicDictionary = json as? Dictionary<String, Any>
+                else{
+                    return
+                    
+            }
+            guard let basicsecondDictionary = basicDictionary["quiz"] as? Dictionary<String, Any>
+                else{
+                    return
+                    
+            }
+            print(basicsecondDictionary)
+            
+//            for user in array {
+//                guard let userDict = user as? [String:Any] else{return}
+//                print(userDict)
+//            }
+        }
+        catch{
+            print(error)
+        }
     }
 
 
